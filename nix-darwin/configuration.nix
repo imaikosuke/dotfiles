0f6f39d1ddc7b0225cf8b6179c1afdd7a54de972
imaikosuke <imaikosuke2@gmail.com>
2026-03-23 00:14:59 +0900

    integrate nix-darwin

---
+++ nix-darwin/configuration.nix
@@ -0,0 +1,15 @@
+{
+  self,
+  ...
+}:
+{
+  users.users."imai".home = "/Users/imai";
+  imports = [
+    ./home_manager.nix
+  ];
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  system.stateVersion = 6;
+  nix.enable = false;
+  security.pam.services.sudo_local.touchIdAuth = true;
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+}
